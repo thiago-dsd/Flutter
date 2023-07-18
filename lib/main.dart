b2b32c51a29cc2d34550a9ebcb3bb76994d0f8df
thiago-dsd <thiago.dsd@outlook.com>
2023-07-18 00:18:05 -0300

    Stack align exemple

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,42 @@
 import 'package:flutter/material.dart';
 
-/*
-  Scaffold - significa andaime em inglês
-*/
-void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Aplicativo Olá Mundo"),
-        ),
-        body:Center(
-          child: Text(
-              "Olá mundo",
-              style: TextStyle(fontSize: 20,color: Colors.black,
-              )
-          ),
-        ),
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(appBar: AppBar(title: Text("Stack App"),),
+        body: Stack(
+          //alignment: AlignmentDirectional.center,
+          children: [
+            Container(
+              width: 200,
+              height: 200,
+              color: Colors.blue,
+            ),
+            Positioned(
+              left: 50,
+              top: 50,
+              //right: -50,
+              child: Container(
+                width: 100,
+                height: 100,
+                color: Colors.red,
+              ),
+            )
+          ],
+        )
       ),
-    ),
-  );
+    );
+  }
 }
