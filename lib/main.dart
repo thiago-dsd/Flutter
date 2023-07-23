03d9736c6928ba8e14d212cd5c69302879736fff
thiago-dsd <thiago.dsd@outlook.com>
2023-07-23 18:54:56 -0300

    Create named routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,105 @@
 import 'package:flutter/material.dart';
 
-/*
-  Scaffold - significa andaime em inglês
-*/
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Aplicativo Olá Mundo"),
-        ),
-        body:Center(
-          child: Text(
-              "Olá mundo",
-              style: TextStyle(fontSize: 20,color: Colors.black,
-              )
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Tela1(),
+        '/tela2': (context) => Tela2(),
+        '/tela3': (context) => Tela3(),
+      },
+    );
+  }
+}
+
+class Tela1 extends StatelessWidget {
+  const Tela1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Tela 1"),
+          ),
+          body: Center(
+              child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/tela2');
+                  },
+                  child: Text("Ir para a tela 2")),
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
+}
+
+class Tela2 extends StatelessWidget {
+  const Tela2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Tela 2"),
+            backgroundColor: Colors.red,
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/');
+                    },
+                    child: Text("Ir para a tela 1")),
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/tela3');
+                    },
+                    child: Text("Ir para a tela 3"))
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Tela3 extends StatelessWidget {
+  const Tela3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Tela 3"),
+          ),
+          body: Center(
+            child: ElevatedButton(
+              child: Text("Ir para a tela 2"),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
