9e75e8ad12fd1534be015bed91eb3cfa09998432
thiago-dsd <thiago.dsd@outlook.com>
2023-07-24 19:28:19 -0300

    Add TextStyle example

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,28 @@
 import 'package:flutter/material.dart';
 
-/*
-  Scaffold - significa andaime em inglês
-*/
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Aplicativo Olá Mundo"),
-        ),
-        body:Center(
-          child: Text(
-              "Olá mundo",
-              style: TextStyle(fontSize: 20,color: Colors.black,
-              )
-          ),
-        ),
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: Text('Texto\nTexto2',
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+              fontSize: 50,
+              height: 1.15, //distance between fonts, proportional
+              fontWeight: FontWeight.normal,
+              fontStyle: FontStyle.italic,
+              color: Colors.black,
+              backgroundColor: Colors.yellow.shade700,
+              decoration: TextDecoration.underline
+            )),
       ),
-    ),
-  );
+    );
+  }
 }
