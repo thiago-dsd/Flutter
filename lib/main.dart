bdaac38b4898dc020f1bd7d4012b1442efee35dc
thiago-dsd <thiago.dsd@outlook.com>
2023-07-25 18:10:11 -0300

    Teste config theme

--- lib/main.dart
+++ lib/main.dart
@@ -9,19 +9,28 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: Text('Texto\nTexto2',
-            textDirection: TextDirection.ltr,
-            style: TextStyle(
-              fontSize: 50,
-              height: 1.15, //distance between fonts, proportional
-              fontWeight: FontWeight.normal,
-              fontStyle: FontStyle.italic,
-              color: Colors.black,
-              backgroundColor: Colors.yellow.shade700,
-              decoration: TextDecoration.underline
-            )),
+    return MaterialApp(
+      theme: ThemeData(
+        //when you subscrite background color in Scaffold DarkMode break
+        brightness: Brightness.light,
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.red,
+        ),
+        canvasColor: Colors.purple,
+      ),
+      home: Scaffold(
+        //backgroundColor: Colors.white70,
+        appBar: AppBar(
+          //backgroundColor: Colors.white10,
+          title: Text("Exemplo"),
+        ),
+        body: Center(
+          child: Text('Texto',
+              style: TextStyle(
+                fontSize: 50,
+                color: Colors.black,
+              )),
+        ),
       ),
     );
   }
