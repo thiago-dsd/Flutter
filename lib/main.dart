830d0a0214a4164f91e9bdf9149561091238911e
thiago-dsd <thiago.dsd@outlook.com>
2023-08-26 17:27:17 -0300

    Create toJson method.

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,38 @@
-import'dart:async';
+import 'dart:async';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-void main(){
-  /* final uri = Uri.https("www.exempla.com", '/', {'q': 'flutter'}); */
-  final uri = Uri.https("www.exempla.com");
+void main() async {
+  Uri uri = Uri.https('jsonplaceholder.typicode.com', '/todos/1');
   final future = http.get(uri);
 
-  print(uri);
-
-  future.then((response){
-    if(response.statusCode == 200){
-      print("Página carregada com sucesso!");
-      print(response.body);
-    }
-    if(response.statusCode == 301){
-      print("Página movida permanentemente!");
-    }
-    if(response.statusCode == 404){
-      print("Página não encontrada!");
-    }
-    if(response.statusCode == 500){
-      print("Erro interno no servidor!");
+  future.then((response) {
+    if (response.statusCode == 200) {
+      Map<String, dynamic> dados = json.decode(response.body);
+      Todo todo = Todo.fromJson(dados);
+      print(todo.toJson());
+    } else {
+      print("Erro");
     }
   });
+}
 
-  future.catchError((onError) => print("Erro!"));
-  future.whenComplete(() => print("Future completo!"));
+class Todo {
+  final int userId;
+  final int id;
+  final String title;
+  final bool completed;
+
+  Todo(this.userId, this.id, this.title, this.completed);
+
+  factory Todo.fromJson(Map<String, dynamic> json) {
+    return Todo(json['userId'], json['id'], json['title'], json['completed']);
+  }
+
+  Map<String, dynamic> toJson() => {
+        'userId': this.userId,
+        'id': this.id,
+        'title': this.title,
+        'completed': this.completed,
+      };
 }
